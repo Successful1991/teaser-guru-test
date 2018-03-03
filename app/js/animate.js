9170625a3b5b82f1d365489725fd5e0ecc9d0d58
Tsys Artem <19successful91@gmail.com>
2018-03-03 17:23:57 +0200

    добавлено меню + анимация

---
+++ app/js/animate.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+  var tl = new TimelineMax();
+
+  tl.from( $(".heading"), 1, { top: -100 },1 )
+    .from( $(".mac__layer1"), 1, { display: 'none' , scale: 0 },"-=0.2" )
+    .from( $(".mac__layer0"), 1, { display: 'none' , scale: 0 },"-=0.2" )
+    .from( $(".mac__layer2"), 1, { display: 'none' , scale: 0 },"-=0.2")
+    .from( $(".mac__layer3"), 1, { display: 'none' , scale: 0 },"-=0.2" )
+    .from( $(".mac__layer4"), 1, { display: 'none' , scale: 0 }, "-=0.2" )
+    .from( $(".navigation__partner"), 1, { display: "none" , top: 600 }, "-=0.2" )
+    .from( $(".navigation__advertiser"), 1, { display: "none" , top: 600 }, "-=0.2" );
+
+
+
+
+
+  $("#menu__burger").click( function () {
+    var inputCheck = $("#menu__burger").prop("checked");
+    console.log("checked = " , inputCheck);
+    if( inputCheck === true ){
+      $(".menu").css("display","flex");
+    }else{
+      $(".menu").css("display","none");
+    }
+  })
+
+
+
+
+
+  });
+
