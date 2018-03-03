1d9811a0d44040565fa5ef4afff15d04f804a38e
Tsys Artem <19successful91@gmail.com>
2018-03-03 20:57:54 +0200

    повравлено для IE

--- app/js/animate.js
+++ app/js/animate.js
@@ -2,14 +2,14 @@ $(document).ready(function(){
 
   var tl = new TimelineMax();
 
-  tl.from( $(".heading"), 1, { top: -100 },1 )
-    .from( $(".mac__layer1"), 1, { display: 'none' , scale: 0 },"-=0.2" )
-    .from( $(".mac__layer0"), 1, { display: 'none' , scale: 0 },"-=0.2" )
-    .from( $(".mac__layer2"), 1, { display: 'none' , scale: 0 },"-=0.2")
-    .from( $(".mac__layer3"), 1, { display: 'none' , scale: 0 },"-=0.2" )
-    .from( $(".mac__layer4"), 1, { display: 'none' , scale: 0 }, "-=0.2" )
-    .from( $(".navigation__partner"), 1, { display: "none" , top: 600 }, "-=0.2" )
-    .from( $(".navigation__advertiser"), 1, { display: "none" , top: 600 }, "-=0.2" );
+  tl.from( $(".heading"), 1, { top: -100, ease: Linear.easeNone },1 )
+    .from( $(".mac__layer1"), 1, { display: 'none' , scale: 0, ease: Linear.easeNone} )
+    .from( $(".mac__layer0"), 1, { display: 'none' , scale: 0, ease: Linear.easeNone} )
+    .from( $(".mac__layer2"), 1, { display: 'none' , scale: 0, ease: Linear.easeNone},"-=0.2")
+    .from( $(".mac__layer3"), 1, { display: 'none' , scale: 0, ease: Linear.easeNone},"-=0.2" )
+    .from( $(".mac__layer4"), 1, { display: 'none' , scale: 0, ease: Linear.easeNone}, "-=0.2" )
+    .from( $(".navigation__partner"), 1, { display: "none" ,top: 600, ease: Linear.easeNone }, "-=0.2" )
+    .from( $(".navigation__advertiser"), 1, { display: "none" , top: 600, ease: Linear.easeNone}, "-=0.2" );
 
 
 
