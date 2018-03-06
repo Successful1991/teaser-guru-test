27dfb20da51d6a2e3d6c50321b23b68ae0442be9
successful1991 <successful1991@gmail.com>
2018-03-06 18:45:55 +0200

    mobile animation

--- app/js/animate.js
+++ app/js/animate.js
@@ -1,17 +1,36 @@
 $(document).ready(function(){
+  // -----------------   setting scroll page   -----------------
+  var scrollifyOptions = {
+    section : ".section-scroll",
+    interstitialSection : "",
+    easing: "easeOutExpo",
+    scrollSpeed: 1000,
+    offset : 110,
+    scrollbars: false,
+    standardScrollElements: "",
+    setHeights: false,
+    overflowScroll: false,
+    updateHash: true,
+    touchScroll:true
+  };
+  if( $(window).width() < 420 ) {
+    $.scrollify(scrollifyOptions);
+  }
+
+
+  // -------------------- setting animation GSAP ------------------
   var tl = new TimelineMax();
   if(window.matchMedia('(min-width: 421px)').matches){
 
   function animateFirstLoadingPage() {
     tl.fromTo( $(".heading"), 1, { y: -300},{y: 0},1 )
-      .from( $(".title__text > h1"), 1, { display: 'none' , fontSize:14 , y: -100, x: 40 , ease: Bounce.easeOut}, "+=0.5" )
+      .from( $(".title__text > h1"), 1, { display: 'none' , y: -100, x: 40 , ease: Bounce.easeOut}, "+=0.5" )
       .from( $(".title__text > p"), 1, { display: 'none' , x: -200, ease: Back.easeOut.config(1.9)} )
       .staggerFromTo($(".title__mac > .mac"), 1,{ display: 'none' , scale: 0}, {display: 'block' , scale: 1}, +0.8)
       .from( $(".navigation__partner"), 0.5, { display: "none" , y:300 } )
       .from( $(".navigation__advertiser"), 0.5, { display: "none" , y:300 });
   }
   animateFirstLoadingPage();
-
 
   $("#menu__burger").click( function () {
     var inputCheck = $("#menu__burger").prop("checked");
@@ -26,7 +45,6 @@ $(document).ready(function(){
     }
   });
 
-
   function openMenu() {
     tl.fromTo($(".heading__logo"), 0,{ display: "flex"}, {display: "none"})
       .fromTo($(".heading"), 0,{ position: "static"}, {position: "relative", justifyContent:"flex-end", zIndex: 1000})
@@ -35,17 +53,16 @@ $(document).ready(function(){
       .staggerFromTo($(".menu__list > li"), 0.4,{ opacity: 0, x: -100}, {opacity: 1, x: 0},0.5);
   }
 
-
   function transitionAnimationFurther() {
     tl.to( $(".mac__layer1"), 2, { rotation: 60 ,y:-600 ,display: 'none'}, "mac" )
       .to( $(".mac__layer0"), 1.8, { rotation: 60 ,y:-600, display: 'none'},"mac" )
       .to( $(".navigation__partner"), 1.5, { rotation: -80 ,y:-900, display: "none", ease: Linear.easeNone }, "-=1.5" )
       .to( $(".navigation__advertiser"), 1.2, { rotation: 80 ,y:-900, display: "none" ,ease: Linear.easeNone }, "-=1.1" )
-      .to( $(".heading__menu"), 1, { yPercent: -300 , display: "none", ease: Linear.easeNone },"-=0.5" )
-      .to( $(".title__text > h1"), 1, { yPercent: -500 , display: "none", ease: Linear.easeNone}, "-=0.5"  )
-      .to( $(".title__text > p"), 0.8, { yPercent: 1200, display: 'none', ease: Linear.easeNone},"-=1")
-      .to( $(".mac__layer2,.mac__layer3,.mac__layer4 "), 1, { animationIterationCount: 0, y: 500 ,display: 'none' },"-=0.5")
-      .to( $(".heading__logo"), 1, { height: 90+"vh", width: 100+"vw" , ease: Linear.easeNone} );
+      .to( $(".heading__menu"), 1, { y: -300 , display: "none", ease: Linear.easeNone },"-=0.5" )
+      .to( $(".title__text > h1"), 1, { y: -500 , display: "none", ease: Linear.easeNone}, "-=0.5"  )
+      .to( $(".title__text > p"), 0.8, { y: 1200, display: 'none', ease: Linear.easeNone},"-=1")
+      .to( $(".mac__layer2,.mac__layer3,.mac__layer4 "), 1, { animationIterationCount: 0, y: 800 ,display: 'none' },"-=0.5")
+      .to( $(".heading__logo"), 1, { height: 90+"vh", width: 100+"vw", position: "absolute" , ease: Linear.easeNone} );
   }
   $(".navigation__partner, .navigation__advertiser").click(function() {transitionAnimationFurther()});
   $(".item__link").mouseover(function () {
@@ -63,7 +80,7 @@ $(document).ready(function(){
   $(".navigation__partner, .navigation__advertiser").mouseout(function () {
     $(this).css("transform","translateY(0)");
   });
-  } // if с matchMedia
+  } // --------------------- animation for screen width max 420 ------------------------
   if(window.matchMedia('(max-width: 420px)').matches){
     $("#menu__burger").click( function () {
       var inputCheck = $("#menu__burger").prop("checked");
@@ -85,6 +102,17 @@ $(document).ready(function(){
       }
     });
   }
+  // ----------------- click to scroll ---------------------------
+
+  $(".title__nextpage").click(function () {
+    tl.to($(".nextpage__img"),1,{ rotation:180}).reverse(10);
+    setTimeout(function () {
+      $.scrollify.next();
+    }, 400);
+
+  });
+
+
 
   });
 
