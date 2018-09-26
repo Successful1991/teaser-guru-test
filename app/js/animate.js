18f1e6e4f34437f8a6f2013ffaa90bd5b13c8844
Tsys Artem <19successful91@gmail.com>
2018-09-26 13:57:55 +0300

    fix time animate

--- app/js/animate.js
+++ app/js/animate.js
@@ -22,14 +22,6 @@ $(document).ready(function(){
   var tl = new TimelineMax();
   if(window.matchMedia('(min-width: 769px)').matches){
 
-  // function animateFirstLoadingPage() {
-  //   tl.fromTo( $(".heading"), 1, { y: -300},{y: 0},1 )
-  //     .from( $(".title__text > h1"), 1, { display: 'none' , y: -100, x: 40 , ease: Bounce.easeOut}, "+=0.5" )
-  //     .from( $(".title__text > p"), 1, { display: 'none' , x: -200, ease: Back.easeOut.config(1.9)} )
-  //     .staggerFromTo($(".title__mac > .mac"), 1,{ display: 'none' , scale: 0}, {display: 'block' , scale: 1}, +0.8)
-  //     .from( $(".navigation__partner"), 0.5, { display: "none" , y:300 } )
-  //     .from( $(".navigation__advertiser"), 0.5, { display: "none" , y:300 });
-  // }
     function animateFirstLoadingPage() {
       tl.fromTo( $(".heading"), 0.5, { y: -300},{y: 0},1 )
         .from( $(".title__text > h1"), 0.5, { opacity: 0, ease: Bounce.easeOut , y: -100 , x:40}, "+=0.2" )
@@ -60,18 +52,6 @@ $(document).ready(function(){
       .fromTo($(".menu__img"), 0.8,{x: -1000},{ x: 0, ease: Linear.easeNone},"+=1")
       .staggerFromTo($(".menu__list > li"), 0.4,{ opacity: 0, x: -100}, {opacity: 1, x: 0},0.5);
   }
-
-  // function transitionAnimationFurther() {
-  //   tl.to( $(".mac__layer1"), 2, { rotation: 60 ,y:-1100 ,display: 'none'}, "mac" )
-  //     .to( $(".mac__layer0"), 1.8, { rotation: 60 ,y:-1100, display: 'none'},"mac" )
-  //     .to( $(".navigation__partner"), 1.5, { rotation: -80 ,y:-2500, display: "none", ease: Power3.easeOut }, "-=1.9" )
-  //     .to( $(".navigation__advertiser"), 1, { rotation: 80 ,y:-2500, display: "none" ,ease: Power3.easeOut }, "-=1" )
-  //     .to( $(".heading__menu"), 1, { y: -500 , display: "none", ease: Linear.easeNone },"-=0.5" )
-  //     .to( $(".title__text > h1"), 1, { y: -800 , display: "none", ease: Linear.easeNone}, "-=0.5"  )
-  //     .to( $(".title__text > p"), 0.8, { y: 1200, display: 'none', ease: Linear.easeNone},"-=1")
-  //     .to( $(".mac__layer2,.mac__layer3,.mac__layer4 "), 1, { animationIterationCount: 0, y: 1300 ,display: 'none', ease:Power3.easeOut },"-=0.5")
-  //     .to( $(".heading__logo"), 1, { top: 40+"vh", left: 35+"vw", position: "absolute" , ease: Linear.easeNone} );
-  // }
 
     function transitionAnimationFurther() {
       tl.to( $(".mac__layer1"), 1, { rotation: 60 ,y:-1100 ,display: 'none'}, "mac" )
